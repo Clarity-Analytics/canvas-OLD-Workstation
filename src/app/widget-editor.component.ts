17adf9505cd219b0ba1841748cfc658de46ab68e
JannieI <jimmelman@gmail.com>
2017-05-23 20:05:05 +0200

    Bugfix: text left validates okay now if = 0

--- src/app/widget-editor.component.ts
+++ src/app/widget-editor.component.ts
@@ -678,10 +678,12 @@ export class WidgetEditorComponent implements OnInit {
             }
             if (this.identificationForm.controls['textLeft'].value == ''  || 
                 this.identificationForm.controls['textLeft'].value == null) {
-                    this.formIsValid = false;
-                    this.numberErrors = this.numberErrors + 1;
-                    this.errorMessageOnForm = this.errorMessageOnForm + ' ' + 
-                        'The Left (Text panel) is compulsory.';
+                    if (this.identificationForm.controls['textLeft'].value != '0') {
+                        this.formIsValid = false;
+                        this.numberErrors = this.numberErrors + 1;
+                        this.errorMessageOnForm = this.errorMessageOnForm + ' ' + 
+                            'The Left (Text panel) is compulsory.';
+                    }
             }                
             if (this.identificationForm.controls['textLeft'].touched  && 
                 !this.identificationForm.controls['textLeft'].valid) {
