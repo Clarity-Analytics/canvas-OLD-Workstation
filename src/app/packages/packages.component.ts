64ba98c8d50d05efb24d1c3650447b5ba65df19d
bradley <bradleykirton@gmail.com>
2017-05-27 16:31:35 +0200

    Some changes to the packages component to allow for a datasource view.

--- src/app/packages/packages.component.ts
+++ src/app/packages/packages.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { EazlPackageService } from '../eazl-package.service';
-import { Package } from '../models/model.package';
+import { Package, Field } from '../models/model.package';
+
+
+interface IComponentPackage extends Package {
+	selected: boolean;
+}
+
 
 @Component({
   selector: 'app-packages',
@@ -8,13 +14,26 @@ import { Package } from '../models/model.package';
   styleUrls: ['./packages.component.css']
 })
 export class PackagesComponent implements OnInit {
-	columns: string[];
-	packageList: Package[];
+	packageList: IComponentPackage[] = [];
+	selectedPackage: IComponentPackage;
+	selectedFields: Field[];
 
 	constructor(
 		private eazlPackages: EazlPackageService)
 
-	{
+	{}
+
+	selectPackage(_package: IComponentPackage){
+		this.packageList.forEach(item => {
+			item.selected = false;
+		});
+
+		_package.selected = true;
+		this.selectedPackage = _package;
+		this.selectedFields = this.selectedPackage.fields;
+	}
+
+	ngOnInit() {
 		this.eazlPackages.packageList.model.subscribe(
 			packages => {
 				if (packages == null) {
@@ -22,8 +41,32 @@ export class PackagesComponent implements OnInit {
 				}
 
 				if (packages.length !== 0) {
-					this.packageList = packages;
-					this.columns = Object.keys(this.packageList[0]);
+					packages.forEach( (item: Package, index: number) => {
+
+						let componentPackage: IComponentPackage = {
+							pk: item.pk,
+							name: item.name,
+							repository: item.repository,
+							parameters: item.parameters,
+							fields: item.fields,
+							queries: item.queries,
+							date_last_synced: item.date_last_synced,
+							last_runtime_error: item.last_runtime_error,
+							last_sync_successful: item.last_sync_successful,
+							last_sync_error: item.last_sync_error,
+							compiled: item.compiled,
+							execute: item.execute,
+							url: item.url,
+							selected: index === 0
+						}
+						
+						if (index === 0) {
+							this.selectedPackage = componentPackage;
+							this.selectedFields = this.selectedPackage.fields;
+						}
+
+						this.packageList.push(componentPackage);
+					});
 				}
 			}, 
 			error => {
@@ -31,7 +74,5 @@ export class PackagesComponent implements OnInit {
 			}
 		);
 	}
-
-	ngOnInit() {}
 
 }
