c9c56a63a0bdd150f0ccaac598a2b2d007787164
JannieI <jimmelman@gmail.com>
2017-06-02 08:36:48 +0200

    Add reportHistory model

---
+++ src/app/model.reportHistory.ts
@@ -0,0 +1,11 @@
+// Schema for Report History (ran by users)
+export class ReportHistory {
+    reportHistoryID: string;                    // Unique ID
+    userName: string;                           // User who ran report
+    reportID: number;                           // Report ID
+    reportHistoryStartDateTime: string;         // Start DateTime
+    reportHistoryEndDateTime: string;           // End DateTime
+    reportHistoryStatus: string;                // Result status: Failed, Success
+    reportHistoryNrRowsReturned: number;        // Nr row returned
+    reportHistoryComments: string;              // Optional Comments
+}
