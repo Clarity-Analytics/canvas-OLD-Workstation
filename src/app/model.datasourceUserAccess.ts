84485083e57fc59d71fa56c35b40ac944edf2d5a
JannieI <jimmelman@gmail.com>
2017-06-16 12:45:08 +0200

    Add EazlDataSourceUserAccess to models

--- src/app/model.datasourceUserAccess.ts
+++ src/app/model.datasourceUserAccess.ts
@@ -1,6 +1,12 @@
 // Schema for the User Access to Data Sources class
 
 // Eazl
+export class EazlDataSourceUserAccess {
+	id: number;                   			// Data Source ID
+	username: string;						// User who has acces
+	type: string;       					// Type = Readonly, Update, Add, Delete, Full
+	scope: string;      					// Applies to: All (records), context specific .. ?
+}
 
 // Canvas
 export class DataSourceUserAccess {
