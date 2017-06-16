60f9efdf90c30461aaf20f9ccc42afa86f061f5a
JannieI <jimmelman@gmail.com>
2017-06-16 12:39:49 +0200

    Add EazlFilter to models

--- src/app/model.filter.ts
+++ src/app/model.filter.ts
@@ -1,7 +1,11 @@
 // Schema for the Dashboard Filter class
 
 // Eazl
-
+export class EazlFilter {
+    has_atleast_one_filter: boolean;
+    owner: string;
+    description: string;
+}
 // Canvas
 export class Filter {
     hasAtLeastOneFilter: boolean;
