c8876bca12e3a193625eea0f9ff3f23d3ad9da86
JannieI <jimmelman@gmail.com>
2017-06-16 13:11:08 +0200

    Add EazlDashboardGroupMembership to models

--- src/app/model.dashboardGroupMembership.ts
+++ src/app/model.dashboardGroupMembership.ts
@@ -1,7 +1,14 @@
 // Schema for the Dashboard-DashboardGroup Membership classes (many - many)
 
 // Eazl
-
+export class EazlDashboardGroupMembership {
+    id: number;
+    dashboard_id: number;
+    updated_on: string;                                       // Updated on
+    updated_by: string;                                       // Updated by
+    created_on: string;                                       // Created on
+    created_by: string;                                       // Created by
+}
 
 // Canvas
 export class DashboardGroupMembership {
