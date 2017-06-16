fec603005516c41c0e5ec31e844092ad7c124e0e
JannieI <jimmelman@gmail.com>
2017-06-16 13:09:55 +0200

    Add EazlDashboardGroupRelationship to models

--- src/app/model.dashboardGroupRelationship.ts
+++ src/app/model.dashboardGroupRelationship.ts
@@ -1,8 +1,17 @@
 // Schema for Dashboard Group Relationship
 
 // Eazl
-
-
+export class EazlDashboardGroupRelationship {
+    id: number;                                             // Unique ID
+    dashboard_id: number;                                   // Dashboard which has relationship
+    group_id: number;                                       // Group ID
+    type: string;                                           // Type of relationship: 
+    rating: number;                                         // Rating
+    updated_on: string;                                     // Updated on
+    updated_by: string;                                     // Updated by
+    created_on: string;                                     // Created on
+    created_by: string;                                     // Created by
+}
 
 // Canvas
 export class DashboardGroupRelationship {
