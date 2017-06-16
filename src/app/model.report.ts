3fe797d747b97e665572972b0675f06cb9d697d8
JannieI <jimmelman@gmail.com>
2017-06-16 12:32:57 +0200

    Add EazlReport to models

--- src/app/model.report.ts
+++ src/app/model.report.ts
@@ -1,7 +1,17 @@
 // Schema for the Report class
 
 // Eazl
+export class EazlReport {
+    id: number;                         // Unique DB ID
+    name: string;                       // Name
+    description: string;                // Description
+    parameters: string;                 // Parameters (optional)
+    datasource_id: number;              // FK to DataSource
+    datasource_parameters: string;      // Data Source Parameters
+    report_fields: string[];            // Array of report fields, obtained from DB
+    report_data: any[];                 // Array (json) of data rows
 
+}
 
 // Canvas
 export class Report {
@@ -13,4 +23,5 @@ export class Report {
     dataSourceParameters: string;       // Data Source Parameters
     reportFields: string[];             // Array of report fields, obtained from DB
     reportData: any[];                  // Array (json) of data rows
+
 }
