7f301533d9ccb9887ee06705a73608d9dfbf01f5
JannieI <jimmelman@gmail.com>
2017-06-16 12:35:29 +0200

    Add EazlNotification to models

--- src/app/model.notification.ts
+++ src/app/model.notification.ts
@@ -1,7 +1,12 @@
 // Schema for the Notification (message layout) class
 
 // Eazl
-
+export class EazlNotification {
+    author: string;
+    date_send: string;
+    message_type: string;
+    message: string;
+}
 
 // Canvas
 export class Notification {
