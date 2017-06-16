be5239500dd68fc18cb8e7c19f97dfc1e80c3713
JannieI <jimmelman@gmail.com>
2017-06-16 12:37:38 +0200

    Add EazlGroupDatasourceAccess to models

--- src/app/model.groupDSaccess.ts
+++ src/app/model.groupDSaccess.ts
@@ -1,15 +1,23 @@
 // Schema for the Group Datasource Access class
 
 // Eazl
-
+export class EazlGroupDatasourceAccess {
+	id: number;                                     // Group ID
+	datasource_id: number;                          // Data Source ID
+	access_type: string;            				// Type = Readonly, Update, Add, Delete, Full
+	created_on: string;       						// Created on
+	created_by: string;         					// Created by
+	updated_on: string;       						// Updated on
+	updated_by: string;         					// Updated by
+}
 
 // Canvas
 export class GroupDatasourceAccess {
-	groupID: number;                                    // Group ID
-	datasourceID: number;                               // Data Source ID
-    groupDatasourceAccessAccessType: string;            // Type = Readonly, Update, Add, Delete, Full
-	groupDatasourceAccessCreatedDateTime: string;       // Created on
-	groupDatasourceAccessCreatedUserID: string;         // Created by
-	groupDatasourceAccessUpdatedDateTime: string;       // Updated on
-	groupDatasourceAccessUpdatedUserID: string;         // Updated by
+	groupID: number;                                // Group ID
+	datasourceID: number;                           // Data Source ID
+    groupDatasourceAccessAccessType: string;        // Type = Readonly, Update, Add, Delete, Full
+	groupDatasourceAccessCreatedDateTime: string;   // Created on
+	groupDatasourceAccessCreatedUserID: string;     // Created by
+	groupDatasourceAccessUpdatedDateTime: string;   // Updated on
+	groupDatasourceAccessUpdatedUserID: string;     // Updated by
 }
