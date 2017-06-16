1a5b5b59f818519ed9dd97583b8120f8ad9bef26
JannieI <jimmelman@gmail.com>
2017-06-16 12:27:11 +0200

    Add EazlReportHistory to models

--- src/app/model.reportHistory.ts
+++ src/app/model.reportHistory.ts
@@ -1,7 +1,17 @@
 // Schema for the Report History (ran by users) class
 
 // Eazl
-
+export class EazlReportHistory {
+    id: number;                                 // Unique ID
+    username: string;                           // User who ran report
+    report_id: number;                          // Report ID
+    datasource_id: number;                      // Underlying Datasource for the report
+    start_on: string;                           // Start DateTime
+    end_on: string;                             // End DateTime
+    status: string;                             // Result status: Failed, Success
+    nr_rows_returned: number;                   // Nr row returned
+    comments: string;                           // Optional Comments
+}
 
 // Canvas
 export class ReportHistory {
