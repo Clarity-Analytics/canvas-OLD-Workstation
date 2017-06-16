c8d239f43196a9225aa500712712d3b26c7ed7a3
JannieI <jimmelman@gmail.com>
2017-06-16 12:21:46 +0200

    Add EazlSelectedItemColor to models

--- src/app/model.selectedItemColor.ts
+++ src/app/model.selectedItemColor.ts
@@ -1,7 +1,11 @@
 // Schema for the colors in PrimeNG dropdowns
 
 // Eazl
-
+export class EazlSelectedItemColor {
+    id: any;
+    name: string;
+    code: string;
+}
 // Canvas
 export class SelectedItemColor {
     id: any;
