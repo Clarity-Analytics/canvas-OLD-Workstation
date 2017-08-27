69e9bf16b688056ccea29a3fbde19fd7ee62714b
JannieI <jimmelman@gmail.com>
2017-08-27 08:58:44 +0200

    Add onChangeChangeGroupPermission method to datasource component

--- src/app/datasource.component.ts
+++ src/app/datasource.component.ts
@@ -676,6 +676,38 @@ export class DataSourceComponent implements OnInit {
         );
     }
 
+    onChangeChangeGroupPermission(event) {
+        // User changed  user permission
+        // - event is the new value of the checkbox
+        this.globalFunctionService.printToConsole(this.constructor.name,'onChangeChangeGroupPermission', '@Start');
+
+        if (this.selectedGroupPermission == null) {
+            this.globalVariableService.growlGlobalMessage.next({
+                severity: 'warn',
+                summary:  'No selection',
+                detail:   'Select a user by clicking the username'
+            });
+
+            return;
+        }
+
+        let assignPermissions: string[] = [];
+        let removePermissions: string[] = [];
+        if (event) {
+            assignPermissions.push('change_package');
+        } else {
+            removePermissions.push('change_package');
+        }
+
+        this.eazlService.updateModelPermissions(
+            'packages',
+            this.selectedDatasource.datasourceID,
+            this.selectedGroupPermission.username,
+            'user',
+            assignPermissions,
+            removePermissions
+        );
+    }
 
 
 
